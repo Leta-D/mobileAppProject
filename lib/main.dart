abfd1078efcf8a27556bcce6ea04e217596efe2e
Leta-D <letadeju.1278@gmail.com>
2025-05-15 21:10:05 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+// import 'package:fitlife2/backend_control/app_state_control.dart';
+// import 'package:flutter/material.dart';
+// import 'package:fitlife2/pages/welcoming/starting.dart';
+// // import 'package:fitlife/pages/welcoming/take_user_info.dart';
+// import 'package:provider/provider.dart';
+// import 'package:fitlife2/pages/main_page.dart';
+
+// void main() {
+//   runApp(
+//     ChangeNotifierProvider(create: (_) => AppStateControl(), child: MyApp()),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "FitLife",
+//       routes: {
+//         // "registry": (context) => TakeRegistry(),
+//         "mainPage": (_) => MainPage(),
+//         // "detailWorkout": (_) => WorkoutShowingPage(),
+//       },
+//       home: StartWelcomePage(),
+//     );
+//   }
+// }
